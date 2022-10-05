5f7daec0b5df987cb1ae2a9166d877aafd000350
harun ünal <hamidullahharun.unal@st.uskudar.edu.tr>
2022-10-05 14:24:25 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+
+namespace AccessArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string hour;
+            string minute;
+            string second;
+            string dayPart = "";
+         
+
+
+            Console.Write("enter time..\n");
+            
+            hour = Console.ReadLine();
+            minute = Console.ReadLine();
+            second = Console.ReadLine();
+            dayPart = Console.ReadLine();
+
+            Console.WriteLine(hour + ":" + minute + ":" + second + dayPart);
+
+            if (dayPart == "PM")
+            {
+                hour = hour + "12";
+                Console.WriteLine("converted time is\n");
+                Console.WriteLine(hour + ":" + minute + ":" + second);
+
+            }
+            else if(dayPart == "AM")
+            {
+                Console.WriteLine(hour + ":" + minute + ":" + second);
+            }
+           
+
+        }
+    }
+}
