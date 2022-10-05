dd052642d1fbaf6ce7a08a83c82d8256a031ec8a
harun ünal <hamidullahharun.unal@st.uskudar.edu.tr>
2022-10-05 16:12:04 +0300

    Proje dosyası ekle.

---
+++ staircase/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+class program
+{
+
+    public static void StairCase(int n)
+    {
+        for (int i = 1; i <= n ;  i++)
+        {
+            
+            for (int j = 1; j <= n ; j++)
+            {
+                if (j <= n - i)
+                {
+                    Console.Write(" ");
+                }
+                else
+                {
+                    Console.Write("#");
+                }
+
+            }
+            Console.WriteLine();
+        }
+        
+    }
+    
+    static void Main(string[] args)
+    {
+        StairCase(6);
+        
+        
+
+    }
+
+
+}
