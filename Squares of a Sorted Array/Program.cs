9de34dc9feb0c8a8edb75dec18bef56ba7e707b4
harun ünal <hamidullahharun.unal@st.uskudar.edu.tr>
2022-10-28 23:56:35 +0300

    Proje dosyası ekle.

---
+++ Squares of a Sorted Array/Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+public class Solution
+{
+    public int[] SortedSquares(int[] nums)
+    {
+        int[] result = new int[nums.Length];
+        List<int> list = new List<int>();
+        
+        int tmp = 0;
+       
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            list.Add(nums[i] * nums[i]);
+
+        }
+        nums = list.ToArray();
+
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length - i - 1; j++)
+            {
+                if (nums[j] > nums[j + 1])
+                {
+                    tmp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = tmp;
+                }
+
+            }
+
+
+        }
+
+        return nums;
+       
+
+    }
+
+    public static void Main(string[] args)
+    {
+        Solution sl = new Solution();
+
+        int[] nums = { -5, -3, -2, -1 };
+        
+
+      Array.ForEach(sl.SortedSquares(nums), Console.WriteLine);
+
+    }
+}
